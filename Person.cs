05b93c0f107da15c077a575045664e14e6d6a4a8
Eles007 <lyos00@mail.ru>
2021-01-15 17:07:22 +0400

    two

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash
+{
+    class Person
+    {
+        public string FIO { get; set; }
+        public int Age;
+        public enum Sex
+        {
+            Female,
+            Male
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash
+{
+    class Service
+    {
+        public string Name;
+        public int Price;
+        public Service(int price, string name)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
